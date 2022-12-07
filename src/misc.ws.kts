922d08bb1ec3588e3a3ffe51269361db129ba50a
Alan Arbizu <alan.arbizu@gmail.com>
2022-12-07 02:13:19 -0800

    day7, pt1 done

--- src/misc.ws.kts
+++ src/misc.ws.kts
@@ -1,3 +1,5 @@
+import java.lang.StringBuilder
+
 (1 .. 26)
 
 ('a' .. 'z').forEach { print(it) }
@@ -21,13 +23,11 @@ v2
 v3
 v4
 
-val regex2 = """\[(\w)\]\s[\s\[]([\s\w])[\s\]][\s\[]([\s\w])[\s\]][\s\[]([\s\w])[\s\]][\s\[]([\s\w])[\s\]][\s\[]([\s\w])[\s\]][\s\[]([\s\w])[\s\]][\s\[]([\s\w])[\s\]][\s\[]([\s\w])[\s\]]""".toRegex()
-regex2
-//val (x1, x2, x3, x4, x5, x6, x7, x8, x9) = regex2.find("[V]         [T]         [J]")!!.destructured
+val s = StringBuilder()
 
+(1..10).forEach { s.append("t")}
 
-
-
+s
 
 
 
