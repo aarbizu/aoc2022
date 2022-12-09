dbdefcd8961a52d1e3f167959e28be81695c1505
Alan Arbizu <alan.arbizu@gmail.com>
2022-12-09 01:12:03 -0800

    day8 done

---
+++ src/main/kotlin/org/aarbizu/aoc2022/dayEight/TreeCover.kt
@@ -0,0 +1,146 @@
+package org.aarbizu.aoc2022.dayEight
+
+import org.aarbizu.aoc2022.util.GridSquare
+import org.aarbizu.aoc2022.util.IntGrid
+import org.aarbizu.aoc2022.util.parseGrid
+
+class TreeCover {
+    fun findVisibleTrees(gridAsStrings: List<String>): List<GridSquare<Int>> {
+        val theGrid = parseGrid(gridAsStrings)
+
+        val perimeterSet = theGrid.perimeter()
+        val toBeEvaluated = theGrid.getGridSet() - perimeterSet
+
+        val filtered = toBeEvaluated.filter { isVisible(it, theGrid) }
+
+        return filtered + perimeterSet
+    }
+
+    fun maxVisibilityScore(gridAsStrings: List<String>): Int {
+        val theGrid = parseGrid(gridAsStrings)
+
+        val perimeterSet = theGrid.perimeter()
+        val toBeEvaluated = theGrid.getGridSet() - perimeterSet
+        return toBeEvaluated.maxOf { vizScore(it, theGrid) }
+    }
+
+    private fun vizScore(tree: GridSquare<Int>, forest: IntGrid): Int {
+        var row = tree.row
+        var col = tree.col
+        val height = tree.value
+
+        var leftScore = 0
+        do {
+            col -= 1
+            if (forest[row,col] != -1) {
+                if (forest[row,col] < height) {
+                    leftScore++
+                } else {
+                    leftScore++
+                    break
+                }
+            }
+        } while (forest[row,col] != -1)
+
+        row = tree.row
+        col = tree.col
+
+        var rightScore = 0
+        do {
+            col += 1
+            if (forest[row,col] != -1) {
+                if (forest[row,col] < height) {
+                    rightScore++
+                } else {
+                    rightScore++
+                    break
+                }
+            }
+        } while (forest[row,col] != -1)
+
+        row = tree.row
+        col = tree.col
+
+        var upScore = 0
+        do {
+            row -= 1
+            if (forest[row,col] != -1) {
+                if (forest[row,col] < height) {
+                    upScore++
+                } else {
+                    upScore++
+                    break
+                }
+            }
+        } while (forest[row,col] != -1)
+
+        row = tree.row
+        col = tree.col
+        var downScore = 0
+        do {
+            row += 1
+            if (forest[row,col] != -1) {
+                if (forest[row,col] < height) {
+                    downScore++
+                } else {
+                    downScore++
+                    break
+                }
+            }
+        } while (forest[row,col] != -1)
+
+        return listOf(leftScore, rightScore, upScore, downScore)
+            .filter { it > 0 }
+            .reduceRight { a: Int, b: Int -> a * b }
+    }
+
+    private fun isVisible(tree: GridSquare<Int>, forest: IntGrid): Boolean {
+        var row = tree.row
+        var col = tree.col
+        val height = tree.value
+
+        // check visibility to left
+        var leftVisible = true
+        do {
+            col -= 1
+            if (forest[row,col] >= height) {
+                leftVisible = false
+            }
+        } while (forest[row,col] != -1)
+
+        row = tree.row
+        col = tree.col
+
+        var rightVisible = true
+        do {
+            col += 1
+            if (forest[row,col] >= height) {
+                rightVisible = false
+            }
+        } while ((forest[row,col] != -1))
+
+        row = tree.row
+        col = tree.col
+
+        var topVisible = true
+        do {
+            row += 1
+            if (forest[row,col] >= height) {
+                topVisible = false
+            }
+        } while ((forest[row,col] != -1))
+
+        row = tree.row
+        col = tree.col
+
+        var bottomVisible = true
+        do {
+            row -= 1
+            if (forest[row,col] >= height) {
+                bottomVisible = false
+            }
+        } while ((forest[row,col] != -1) )
+
+        return leftVisible || rightVisible || topVisible || bottomVisible
+    }
+}
---
+++ src/test/kotlin/org/aarbizu/aoc2022/dayEight/TreeCoverTest.kt
@@ -0,0 +1,70 @@
+package org.aarbizu.aoc2022.dayEight
+
+import com.google.common.truth.Truth.assertThat
+import org.aarbizu.aoc2022.util.INPUTS_DIR
+import org.aarbizu.aoc2022.util.parseGrid
+import org.junit.jupiter.api.Test
+import java.io.File
+
+class TreeCoverTest {
+
+    @Test
+    fun `parsing to grid`() {
+        val grid = parseGrid(sample.lines())
+        assertThat(grid).isNotNull()
+    }
+
+    @Test
+    fun `count perimeter`() {
+        val grid = parseGrid(sample.lines())
+        assertThat(grid.perimeter().count())
+            .isEqualTo(16)
+            .also { println("visible (perimeter trees) = ${grid.perimeter().count()}") }
+    }
+
+    @Test
+    fun `count visible in sample`() {
+        val grid = parseGrid(sample.lines())
+        val gridSet = grid.getGridSet()
+        assertThat(gridSet.size).isEqualTo(25)
+
+        val perimeterSet = grid.perimeter()
+        val toBeEvaluated = gridSet - perimeterSet
+        assertThat(toBeEvaluated.size).isEqualTo(9)
+        assertThat(toBeEvaluated.map { it.value }).containsExactly(5,5,1,5,3,3,3,5,4)
+    }
+
+    @Test
+    fun sample () {
+        val visible = TreeCover().findVisibleTrees(sample.lines())
+        assertThat(visible.size).isEqualTo(21)
+    }
+
+    @Test
+    fun part1() {
+        val treeGridStrings = File("$INPUTS_DIR/d8-p1.txt").readLines()
+        val visible = TreeCover().findVisibleTrees(treeGridStrings)
+        assertThat(visible.size).isEqualTo(1679).also { println("visible tree count = ${visible.size}") }
+    }
+
+    @Test
+    fun `sample best visibility score of all trees`() {
+        val maxVisibility = TreeCover().maxVisibilityScore(sample.lines())
+        assertThat(maxVisibility).isEqualTo(8)
+    }
+
+    @Test
+    fun part2() {
+        val treeGridStrings = File("$INPUTS_DIR/d8-p1.txt").readLines()
+        val maxVisibility = TreeCover().maxVisibilityScore(treeGridStrings)
+        assertThat(maxVisibility).isEqualTo(536625).also { println("max visibility = $maxVisibility") }
+    }
+
+    val sample = """
+        30373
+        25512
+        65332
+        33549
+        35390
+    """.trimIndent()
+}
